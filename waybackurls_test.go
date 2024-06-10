d8007992dc4e3f24ae12cd30a8f04c094221956b
whitecat1331 <whitecat1331@gmail.com>
2024-06-10 11:00:27 -0400

    Update tests

--- waybackurls_test.go
+++ waybackurls_test.go
@@ -1,16 +1,33 @@
 package waybackurls
 
 import (
-	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
+type testValues struct {
+	Domain  string
+	Domains []string
+	Output  string
+}
+
+func NewTestingValues() testValues {
+	domains := strings.Split(os.Getenv("DOMAINS"), ",")
+	return testValues{
+		Domain:  domains[0],
+		Domains: domains,
+		Output:  os.Getenv("OUTPUT"),
+	}
+
+}
+
+var tv = NewTestingValues()
+
 func TestWaybackURLS(t *testing.T) {
-	domains := []string{os.Getenv("DOMAIN")}
-	fmt.Println(domains)
-	wurls := WaybackURLS([]string{""})
-	fmt.Println(wurls)
+	t.Log(tv.Domains)
+	wurls := WaybackURLS(tv.Domains)
+	t.Log(wurls)
 }
